34ae1fe87ba839d8de1946bcff3bea92edfd2412
SINGH, KULDIP [AG/1000] <ksingh1@c02qt0lug8wp.na.ds.monsanto.com>
2016-02-12 17:17:04 -0600

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by SINGH, KULDIP [AG/1000] on 2/12/16.
+//  Copyright © 2016 SINGH, KULDIP [AG/1000]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
